da8ac021fa21edac1d2646bde7a7cb177a01a649
Jamison Burton <jamison@burtontalk.com>
2022-01-18 08:53:07 -0700

    Wrote code

---
+++ ForLoopExample/ForLoopExample.vb
@@ -0,0 +1,27 @@
+﻿Option Explicit On
+Option Strict On
+Module ForLoopExample
+    Sub Main()
+
+        ''For i = 1 to 10
+        'For index As Integer = 1 To 10 Step 1
+        '    Console.WriteLine("Something" & " " & CStr(index))
+        'Next
+
+        'Dim total As Integer
+        'For i = 1 To 10
+        '    'total = total + 2
+        '    total += 2
+        '    Console.WriteLine("The total is:" & " " & CStr(total))
+        'Next
+
+        Dim star As String = ""
+        For i = 1 To 10
+            star &= "*"
+            Console.WriteLine(star)
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
